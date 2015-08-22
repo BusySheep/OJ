3ce17983200d5d5ede0119d2d3cdcd3219d32f73
LoserKe <kehycs@foxmail.com>
2015-08-23 07:29:40 +0800

    add 263

---
+++ Leetcode/263.cpp
@@ -0,0 +1,30 @@
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return num == 1;
+    }
+};
+int main() {
+    int n;
+    cin >> n;
+    cout << Solution().isUgly(n) << endl;
+    return 0;
+}
