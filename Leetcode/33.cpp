ec9af7395e48747369b12af742a984b8758571cd
LoserKe <kehycs@foxmail.com>
2015-09-05 15:15:48 +0800

    modify 33

--- Leetcode/33.cpp
+++ Leetcode/33.cpp
@@ -4,23 +4,25 @@ public:
         int l = 0;
         int r = nums.size();
         while (l < r) {
-        	int mid = (l + r) / 2;
-        	if (nums[mid] == target) return mid;
-        	if (nums[mid] >= nums[l]) {
-        		// the left area is normally sorted
-        		if (target >= nums[l] && target < nums[mid]) {
-        			r = mid;
-        		} else {
-        			l = mid + 1;
-        		}
-        	} else {
-        		// the right area is normally sorted
-        		if (target <= nums[r - 1] && target > nums[mid]) {
-        			l = mid + 1;
-        		} else {
-        			r = mid;
-        		}
-        	}
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[l] == target) return l;
+            if (nums[r - 1] == target) return r - 1;
+            if (nums[mid] >= nums[l]) {
+                // the left area is normally sorted
+                if (target >= nums[l] && target < nums[mid]) {
+                    r = mid;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                // the right area is normally sorted
+                if (target <= nums[r - 1] && target > nums[mid]) {
+                    l = mid + 1;
+                } else {
+                    r = mid;
+                }
+            }
         }
         return -1;
     }
