2b9d90a8ec50199658b8daf851d11642b20c99a4
LoserKe <kehycs@foxmail.com>
2015-09-08 19:49:04 +0800

    modify 1093

--- PAT/Advanced Level/1093.cpp
+++ PAT/Advanced Level/1093.cpp
@@ -1,29 +1,32 @@
 #include <cstdio>
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <string>
 using namespace std;
 
 int main() {
     string str;
     cin >> str;
-    string pattern = "PAT";
-    vector<int> cnt(str.size(), 1);
-    for (int i = 0; i < pattern.size(); i++) {
-        int count = 0;
-        for (auto j = 0; j < str.size(); j++) {
-            if (str[j] == pattern[i]) {
-                count += cnt[j];
-                count %= 1000000007;
-            }
-            cnt[j] = count;
+    long ans = 0;
+    vector<int> countP(str.length(), 0);
+    vector<int> countT(str.length(), 0);
+    if (str.length() == 0) goto OUT;
+    countP[0] = str[0] == 'P';
+    countT[countT.size() - 1] = str[str.length() - 1] == 'T';
+    for (int i = 1; i < str.length(); i++) {
+        if (str[i] == 'P') countP[i] = countP[i - 1] + 1;
+        else countP[i] = countP[i - 1];
+    }
+    for (int i = str.length() - 2; i >= 0; i--) {
+        if (str[i] == 'T') countT[i] = countT[i + 1] + 1;
+        else countT[i] = countT[i + 1];
+        if (str[i] == 'A') {
+            ans += countP[i] * countT[i];
+            ans %= 1000000007;
         }
     }
-    if (str.size() == 0) {
-        printf("0\n");
-    }
-    else {
-        printf("%d\n", cnt[str.size() - 1]);
-    }
+    OUT:
+    printf("%ld\n", ans);
     return 0;
 }
