51e214c30dc39f18f9e1cf810d45dcb32f5e656c
LoserKe <kehycs@foxmail.com>
2015-09-08 19:35:08 +0800

    modify 1092

--- PAT/Advanced Level/1092.cpp
+++ PAT/Advanced Level/1092.cpp
@@ -1,54 +1,32 @@
+#include <iostream>
 #include <cstdio>
 #include <algorithm>
 #include <vector>
-#include <cstring>
+#include <string>
 using namespace std;
 
-const int N = 10 + 26 + 26;
-
-int colorNumber(char color) {
-    if (color >= '0' && color <= '9') {
-        return color - '0';
-    }
-    else if (color >= 'a' && color <= 'z') {
-        return color - 'a' + 10;
-    }
-    else {
-        return color - 'A' + 10 + 26;
-    }
-}
-
 int main() {
-    char beads[1001];
-    int color1[N];
-    int color2[N];
-    memset(color1, 0, N * sizeof(int));
-    memset(color2, 0, N * sizeof(int));
-    gets(beads);
-    for (int i = 0; beads[i]; i++) {
-        color1[colorNumber(beads[i])]++;
+    char sellBead[1001];
+    char needBead[1001];
+    gets(sellBead);
+    gets(needBead);
+    vector<int> have(128, 0);
+    for (int i = 0; sellBead[i]; i++) {
+        have[sellBead[i]]++;
     }
-    gets(beads);
-    for (int i = 0; beads[i]; i++) {
-        color2[colorNumber(beads[i])]++;
+    for (int i = 0; needBead[i]; i++) {
+        have[needBead[i]]--;
     }
-    int count1 = 0;
-    int count2 = 0;
-    bool flag = false;
-    for (int i = 0; i < N; i++) {
-        if (color1[i] < color2[i]) {
-            flag = true;
-            count2 += color2[i] - color1[i];
-        }
-        else {
-            count1 += color1[i] - color2[i];
-        }
+    int extra = 0;
+    int lack = 0;
+    for (int i = 0; i < 128; i++) {
+        if (have[i] > 0) extra += have[i];
+        else if (have[i] < 0) lack -= have[i];
     }
-    if (!flag) {
-        printf("Yes %d\n", count1);
-    }
-    else {
-        printf("No %d\n", count2);
+    if (lack == 0) {
+        printf("Yes %d\n", extra);
+    } else {
+        printf("No %d\n", lack);
     }
     return 0;
 }
