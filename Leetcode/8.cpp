36626dfed3f4155d81b44d9c505f7382716c7a31
LoserKe <kehycs@foxmail.com>
2015-10-17 11:34:26 +0800

    modify 8

--- Leetcode/8.cpp
+++ Leetcode/8.cpp
@@ -1,70 +1,41 @@
-#include <iostream>
-#include <string>
-#include <climits>
-using namespace std;
-
 class Solution {
-public:
-    string trim(string str) {
-        for (int i = str.length() - 1; i >= 0; i--) {
-            if (str[i] == ' ') {
-                str.pop_back();
-            } else {
-                break;
-            }
-        }
-        int i;
-        for (i = 0; i < str.length(); i++) {
-            if (str[i] != ' ') {
-                break;
-            }
-        }
-        return str.substr(i, str.length() - i);
+private:
+    string trim(string s) {
+        int begin = 0;
+        while (s[begin] == ' ') begin++;
+        int end = s.length() - 1;
+        while (s[end] == ' ') end--;
+        return s.substr(begin, end + 1 - begin);
     }
+    int charToInt(char c) {
+        if (c >= '0' && c <= '9') return c - '0';
+        else return -1;
+    }
+    int genResult(long result, int sign) {
+        result *= sign;
+        if (result > INT_MAX) return INT_MAX;
+        else if (result < INT_MIN) return INT_MIN;
+        else return result;
+    }
+public:
     int myAtoi(string str) {
         str = trim(str);
-        if (str == "") {
-            return 0;
+        int sign = 1;
+        int i = 0;
+        if (str[0] == '-') {
+            sign = -1;
+            i++;
+        } else if (str[0] == '+') {
+            i++;
         }
-        int result = 0;
-        bool negativeFlag = false;
-        bool signFlag = false;
-        bool numberFlag = false;
-        for (int i = 0; i < str.length(); i++) {
-            if (str[i] == '-' || str[i] == '+') {
-                if (signFlag == true) {
-                    return 0;
-                } else if (numberFlag == true) {
-                    return result;
-                }
-                signFlag = true;
-                if (str[i] == '-') {
-                    negativeFlag = true;
-                }
-            } else if (str[i] <= '9' && str[i] >= '0') {
-                numberFlag = true;
-                if (negativeFlag) {
-                    if ((INT_MIN + (str[i] - '0')) / 10 > result) {
-                        return INT_MIN;
-                    }
-                    result = result * 10 - (str[i] - '0');
-                } else {
-                    if ((INT_MAX - (str[i] - '0')) / 10 < result) {
-                        return INT_MAX;
-                    }
-                    result = result * 10 + (str[i] - '0');
-                }
-            } else {
-                return result;
-            }
+        long result = 0;
+        for (; i < str.length(); i++) {
+            int temp = charToInt(str[i]);
+            if (temp == -1) return genResult(result, sign);
+            result *= 10;
+            result += temp;
+            if (result > 10000000000L) return genResult(result, sign);
         }
-        return result;
+        return genResult(result, sign);
     }
 };
-
-int main() {
-    string str;
-    cin >> str;
-    cout << Solution().myAtoi(str) << endl;
-	return 0;
-}
