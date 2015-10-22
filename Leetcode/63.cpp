792b94d5aa9ba775f18783fc91e0499a13651737
LoserKe <kehycs@foxmail.com>
2015-10-23 02:00:06 +0800

    add 63

--- Leetcode/63.cpp
+++ Leetcode/63.cpp
@@ -4,22 +4,17 @@ public:
         int m = obstacleGrid.size();
         if (m == 0) return 0;
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[0][0] = obstacleGrid[0][0] == 0;
+        if (n == 0) return 0;
+        vector<int> dp(n);
+        dp[0] = !obstacleGrid[0][0];
+        for (int i = 1; i < n; i++) dp[i] = obstacleGrid[0][i] ? 0 : dp[i - 1];
         for (int i = 1; i < m; i++) {
-            if (obstacleGrid[i][0]) continue;
-            dp[i][0] = dp[i - 1][0];
-        }
-        for (int i = 1; i < n; i++) {
-            if (obstacleGrid[0][i]) continue;
-            dp[0][i] = dp[0][i - 1];
-        }
-        for (int i = 1; i < m; i++) {
+            dp[0] = obstacleGrid[i][0] ? 0 : dp[0];
             for (int j = 1; j < n; j++) {
-                if (obstacleGrid[i][j]) continue;
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                if (obstacleGrid[i][j]) dp[j] = 0;
+                else dp[j] += dp[j - 1];
             }
         }
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 };
