d7288e1e693f5faf133cab9bb033fd84dc332e31
LoserKe <kehycs@foxmail.com>
2015-10-23 23:10:37 +0800

    modify 11

--- Leetcode/11.cpp
+++ Leetcode/11.cpp
@@ -1,37 +1,14 @@
-#include <iostream>
-#include <string>
-#include <climits>
-#include <queue>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        int result = 0;
         int i = 0;
         int j = height.size() - 1;
-        int max = 0;
         while (i < j) {
-            int area = min(height[i], height[j]) * (j - i);
-            if (area > max) {
-                max = area;
-            }
-            if (height[i] < height[j]) {
-                i++;
-            } else {
-                j--;
-            }
+            result = max(result, min(height[i], height[j]) * (j - i));
+            if (height[i] < height[j]) i++;
+            else j--;
         }
-        return max;
+        return result;
     }
 };
-int main() {
-    int n = 15000;
-    // cin >> n;
-    vector<int> height(n, 0);
-    for (int i = 0; i < n; i++) {
-        height[i] = n - i;
-    }
-    cout << Solution().maxArea(height) << endl;
-	return 0;
-}
