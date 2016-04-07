493f4fa9776bff45e62d1880ee334055d1baf9cc
SnowyOwl-KHY <kehycs@foxmail.com>
2016-04-08 01:08:36 +0800

    modify zoj 3702

--- ZOJ/3702.cpp
+++ ZOJ/3702.cpp
@@ -1,46 +1,38 @@
-#include <cstdio>
+#include <vector>
 #include <algorithm>
+#include <cstdio>
 using namespace std;
 
-int coe[21];
-int constant[21];
-
+long f[21];
 void init() {
-    coe[0] = coe[1] = coe[2] = 1;
-    constant[0] = 1;
-    constant[1] = 0;
-    constant[2] = 1;
-    for (int i = 3; i < 21; i++) {
-        coe[i] = coe[i - 1] + coe[i - 2];
-        constant[i] = constant[i - 1] + constant[i - 2];
-    }
+	f[0] = f[1] = 1;
+	for (int i = 2; i < 21; ++i) {
+		f[i] = f[i - 1] + f[i - 2];
+	}
 }
 
 int main() {
-    init();
-    int T;
-    scanf("%d", &T);
-    while (T--) {
-        int i;
-        int gi;
-        int j;
-        scanf("%d%d%d", &i, &gi, &j);
-        int t = 0;
-        if ((gi - constant[i]) % coe[i] == 0) {
-            t = (gi - constant[i]) / coe[i];
-        }
-        if (t <= 0) {
-            printf("-1\n");
-        }
-        else {
-            long g[21];
-            g[0] = 1;
-            g[1] = t;
-            for (int index = 2; index <= j; index++) {
-                g[index] = g[index - 1] + g[index - 2];
-            }
-            printf("%ld\n", g[j]);
-        }
-    }
-    return 0;
+	init();
+	int T;
+	scanf("%d", &T);
+	while (T--) {
+		int i, gi, j;
+		scanf("%d%d%d", &i, &gi, &j);
+		int t = -1;
+		if ((gi - f[i - 2]) % f[i - 1] == 0) {
+			t = (gi - f[i - 2]) / f[i - 1];
+		}
+		if (t < 1) {
+			puts("-1");
+		} else {
+			long g[21];
+			g[0] = 1;
+			g[1] = t;
+			for (int index = 2; index <= j; ++index) {
+				g[index] = g[index - 1] + g[index - 2];
+			}
+			printf("%ld\n", g[j]);
+		}
+	}
+	return 0;
 }
